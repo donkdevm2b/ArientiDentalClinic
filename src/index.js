acf229a420c8a1c11f634e06f2254704c6ae02b8
Matteo Michele Bianchini <matteom.bianchini@gmail.com>
2018-05-17 19:14:36 +0200

    menu almost finished

--- src/index.js
+++ src/index.js
@@ -13,24 +13,36 @@ function menuTrigger () {
 
 // hamburger-button click
 document.getElementById('hamburger-button').onclick = function () {
-  var nowClass = document.getElementById('hamburger-button').innerHTML
+  var menuSection = document.getElementById('menu-section')
+  var hamburgerButton = document.getElementById('hamburger-button')
+  console.log(menuSection.offsetHeight)
+  var nowClass = hamburgerButton.innerHTML
   var isOpening = nowClass === 'menu'
   // change hb icon
-  document.getElementById('hamburger-button').innerHTML = isOpening ? 'close' : 'menu'
+  hamburgerButton.innerHTML = isOpening ? 'close' : 'menu'
   // open | close section menu
-  var animation = isOpening ? 
-  [
-    { height: '120px' },
-    { height: '500px' },
-  ] :
-  [
-    { height: '500px' },
-    { height: '120px' },
-  ]
+  var headerStartHeight = 120
+  // var headerEndHeight = menuSection.offsetHeight
+  var startHeight = headerStartHeight + 'px'
+  var endHeight = headerStartHeight + menuSection.offsetHeight + 'px'
 
-  document.getElementsByTagName('header')[0].animate(animation, 1000)
+  console.log(startHeight, endHeight)
+
+  var finalPosition = isOpening ? endHeight : startHeight
+
+  document.getElementsByTagName('header')[0].style.height = finalPosition
   // show menu section
-  document.getElementById("menu-section").classList.toggle("is-off")
+  // document.getElementById("menu-section").classList.toggle("is-off")
+  // var animation2 = isOpening ?
+  //   [
+  //     { height: '120px' },
+  //     { height: '500px' },
+  //   ] :
+  //   [
+  //     { height: '500px' },
+  //     { height: '120px' },
+  //   ]
+  // document.getElementById("menu-section").animate(animation2, 1000)
     
 }
 
