16030fe55171979a43b449ff84db83d137389752
Matteo Michele Bianchini <matteom.bianchini@gmail.com>
2018-05-17 19:33:01 +0200

    close menu section if it's open

--- src/index.js
+++ src/index.js
@@ -13,38 +13,36 @@ function menuTrigger () {
 
 // hamburger-button click
 document.getElementById('hamburger-button').onclick = function () {
+  handleMenuClick() 
+}
+
+function menuIsOpen() {
+  return document.getElementById('hamburger-button').innerHTML === 'close'
+}
+
+function handleMenuClick () {
   var menuSection = document.getElementById('menu-section')
   var hamburgerButton = document.getElementById('hamburger-button')
-  console.log(menuSection.offsetHeight)
   var nowClass = hamburgerButton.innerHTML
   var isOpening = nowClass === 'menu'
   // change hb icon
   hamburgerButton.innerHTML = isOpening ? 'close' : 'menu'
-  // open | close section menu
+  // open | close header height
   var headerStartHeight = 120
-  // var headerEndHeight = menuSection.offsetHeight
   var startHeight = headerStartHeight + 'px'
   var endHeight = headerStartHeight + menuSection.offsetHeight + 'px'
-
-  console.log(startHeight, endHeight)
-
   var finalPosition = isOpening ? endHeight : startHeight
-
-  document.getElementsByTagName('header')[0].style.height = finalPosition
-  // show menu section
-  // document.getElementById("menu-section").classList.toggle("is-off")
-  // var animation2 = isOpening ?
-  //   [
-  //     { height: '120px' },
-  //     { height: '500px' },
-  //   ] :
-  //   [
-  //     { height: '500px' },
-  //     { height: '120px' },
-  //   ]
-  // document.getElementById("menu-section").animate(animation2, 1000)
-    
+  document.getElementsByTagName('header')[0].style.height = finalPosition   
 }
+
+window.addEventListener('resize', function () { 
+  console.log('resize')
+  // close menu section if it's open
+  if (menuIsOpen()) {
+    handleMenuClick()
+  }
+})
+
 
 // function scrollToId(id) { $('html,body').animate({ scrollTop: $(id).offset().top - headerHeight }, 500) }
 
