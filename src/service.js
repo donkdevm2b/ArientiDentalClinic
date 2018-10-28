a570ef1491a0f94168490f2cdffc8dd928128fb0
Matteo Michele Bianchini <matteom.bianchini@gmail.com>
2018-10-28 20:27:13 +0100

    add odontoiatria pediatrica

--- src/service.js
+++ src/service.js
@@ -1,10 +1,31 @@
 import './style/style.scss'
 const menu = require('./menu.js')
 
-const services = ['ortodonzia-invisibile']
+const services = ['ortodonzia-invisibile', 'odontoiatria-pediatrica']
+
+function handleNavigation(direction) {
+  // get actual location
+  const location = window.location.pathname.split('/').pop()
+    .replace('.html', '')
+  const index = services.indexOf(location)
+  let next
+  if (direction === 'next') {
+    next = services[index + 1] || services[0]
+  } else {
+    next = services[index - 1] || services[services.length - 1]
+  }
+  window.location = `/servizi/${next}.html`
+}
 
 window.onload = () => {
   document.getElementsByTagName('body')[0].classList.toggle('is-off')
   $('#footerInjection').load('/html/footer.html')
   $('#menuInjection').load('/html/menu.html', () => { menu.init() })
+
+  document.getElementById('nav-left').onclick = () => {
+    handleNavigation('prev')
+  }
+  document.getElementById('nav-right').onclick = () => {
+    handleNavigation('next')
+  }
 }
