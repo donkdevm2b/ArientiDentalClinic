95acdc86906130cc1e35efa0995c0bd4ee3d9d14
Matteo Michele Bianchini <matteom.bianchini@gmail.com>
2018-11-01 15:22:38 +0100

    hide video on mobile and init layout

--- src/video.js
+++ src/video.js
@@ -1,27 +1,33 @@
 function init() {
-  console.log('VIDEO SCRIPT')
-  const tag = document.createElement('script')
-  tag.src = 'https://www.youtube.com/iframe_api'
-  const [firstScriptTag] = document.getElementsByTagName('script')
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+  if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // is not mobile..
+    console.log('VIDEO SCRIPT')
+    const tag = document.createElement('script')
+    tag.src = 'https://www.youtube.com/iframe_api'
+    const [firstScriptTag] = document.getElementsByTagName('script')
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
-  window.onYouTubeIframeAPIReady = () => {
-    console.log('onYouTubeIframeAPIReady')
-    const player = new YT.Player('video1', {
-      // videoId: '6So3Jru5Y1w',
-      videoId: 'i7PniwJqyz0',
-      playerVars: {
-        controls: 0,
-        modestbranding: 1,
-        showinfo: 0,
-        hl: 'it',
-        rel: 0,
-        autoplay: 1,
-        mute: 1,
-        fs: 0,
-        loop: 1,
-        playlist: 'i7PniwJqyz0' }
-    })
+    window.onYouTubeIframeAPIReady = () => {
+      console.log('onYouTubeIframeAPIReady')
+      const player = new YT.Player('video1', {
+        // videoId: '6So3Jru5Y1w',
+        videoId: 'i7PniwJqyz0',
+        playerVars: {
+          controls: 0,
+          modestbranding: 1,
+          showinfo: 0,
+          hl: 'it',
+          rel: 0,
+          autoplay: 1,
+          mute: 1,
+          fs: 0,
+          loop: 1,
+          playlist: 'i7PniwJqyz0' }
+      })
+    }
+  } else {
+    // is mobile
+    document.getElementsByClassName('video-background')[0].style.visibility = 'hidden'
   }
 }
 
