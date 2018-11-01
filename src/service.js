273180b23da3214e98b51667f325cce067278d72
Matteo Michele Bianchini <matteom.bianchini@gmail.com>
2018-11-01 18:24:31 +0100

    add ortodonzia

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
 import './style/style.scss'
 const menu = require('./menu.js')
 
-const services = ['ortodonzia-invisibile', 'odontoiatria-pediatrica', 'cosmetic-dentistry', 'sedazione-cosciente']
+const services = ['ortodonzia-invisibile', 'odontoiatria-pediatrica', 'cosmetic-dentistry', 'sedazione-cosciente', 'ortodonzia']
 
 function handleNavigation(direction) {
   // get actual location
